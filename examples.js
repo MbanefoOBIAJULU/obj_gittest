22328d9df10abf8fb38375e1d06b70e380be886c
mbanefoobiajulu <dibiaezueobiajulu@yahoo.com>
2024-03-19 11:32:22 +0100

    obiajulu.html

--- examples.js
+++ examples.js
@@ -1,4 +1,4 @@
-// Examples of numeric literals 1, 2, -3, 4.5, -6.77, 234891234 
+/*// Examples of numeric literals 1, 2, -3, 4.5, -6.77, 234891234 
 let num1 = 1
 let num2 = 2
 let num3 = -3
@@ -6,4 +6,9 @@ let num4  = 4
 let num5 = -6.77
 let num6 = 234891234
 console.log(num1, num2,  num3, num4,num5,num6)
-console.log (typeof(num1))
+console.log (typeof(num1))*/
+
+let girl= (10*10*10)
+console.log(girl)
+console.log(typeof(girl))
+
