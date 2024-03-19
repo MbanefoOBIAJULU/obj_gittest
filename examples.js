ae6fe28267a7716c4723ada6a1bf03be9c54cc31
mbanefoobiajulu <dibiaezueobiajulu@yahoo.com>
2024-03-19 12:14:16 +0100

    examples.js

--- examples.js
+++ examples.js
@@ -8,7 +8,19 @@ let num6 = 234891234
 console.log(num1, num2,  num3, num4,num5,num6)
 console.log (typeof(num1))*/
 
-let girl= (10*10*10)
+/*let girl= (10*10*10)
 console.log(girl)
 console.log(typeof(girl))
+*/
+
+/*let shop= (100*27-1000 - (2*10+4+1000-10))
+console.log(shop) 
+console.log(typeof(shop))*/
+
+const excel = [
+{number: 1, maritalstatus: "single", children:1, education: "partial college",  occupation: "skilled manual",},
+{number: 2, maritalstatus: "married", children:4, education: "college",  occupation: "banker",},
+{ number: 3, maritalstatus: "divorced", children:2, education: "medical school",  occupation: "doctor",},
+]
+console.table (excel)
 
